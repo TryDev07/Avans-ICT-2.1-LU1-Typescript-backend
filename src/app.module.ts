124d49cc514ae69e86eb715d30e820d3bbf1f34c
TryDev07 <r.vanleeuwerden@outlook.com>
2025-10-08 00:30:20 +0200

    Fixed big issue.

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,6 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { VkmsModule } from './vkms/vkms.module';
 
 @Module({
-  imports: [VkmsModule, MongooseModule.forRoot('mongodb+srv://rvanleeuwerden_db_user:jnrEsTeu5UYC39o1@avans.pksgofw.mongodb.net/lu2-db')],
+  imports: [VkmsModule, MongooseModule.forRoot(process.env.MONGODB_URI || 'mongodb://localhost/nest')],
 })
 export class AppModule {}
